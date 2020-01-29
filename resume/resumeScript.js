90b6e7120059165c0ab067ec37ff6dec32ac4c6f
Billuc <billuc@hotmail.fr>
2020-01-29 22:40:24 +0100

    JS du CV réparé :p

--- resume/resumeScript.js
+++ resume/resumeScript.js
@@ -8,34 +8,31 @@ function loadXMLDoc(xmlUrl, pAsync) {
 }
 
 function displayResult() {
-	var xsltProcessor = new XSLTProcessor();
-
-	var adress = "localhost:8080/Billuc";
-	
+	var xsltProcessor = new XSLTProcessor();	
 	var xsl;
 	var xml;
-	
-	var xslAjax = loadXMLDoc("/Billuc/resume/HTMLResume.xsl", true);
-	
+
+	var xslAjax = loadXMLDoc("./resume/HTMLResume.xsl", true);
+
 	xslAjax.done(function(data1) {
 		xsl = data1;
 
 		xsltProcessor.importStylesheet(xsl);
-		
-		var xmlAjax = loadXMLDoc("/Billuc/resume/resumeLucB.xml", true);
+
+		var xmlAjax = loadXMLDoc("./resume/resumeLucB.xml", true);
 		xmlAjax.done(function(data2) {
 			xml = data2;
-			
+
 			var newXml = xsltProcessor.transformToDocument(xml);
-			
+
 			var newHead = newXml.head.children;
 			var newBody = newXml.body.children;
-			
+
 			document.body.innerHTML = "";
-			
+
 			//Partie moche ou l'on sort les éléments de newBody pour les mettre dans le *
 			// tableau elements que l'on parcourt pour ajouter les elements au body !
-			// Pour une raison inconnue, l'ajout direct au body bugue et seuls 3 des 5 
+			// Pour une raison inconnue, l'ajout direct au body bugue et seuls 3 des 5
 			// parties sont ajoutées !
 			goThroughArrayAndAddTo(newBody, $("body"));
 			goThroughArrayAndAddTo(newHead, $("head"));
@@ -45,12 +42,12 @@ function displayResult() {
 
 function goThroughArrayAndAddTo(pArray, pAddTo) {
 	var elements = [];
-	
+
 	for (var i = 0; i < pArray.length; i++) {
 		var element = pArray[i];
-		elements.push(element);							
+		elements.push(element);
 	}
-	
+
 	elements.forEach(function(item, index, array) {
 		pAddTo.append(item);
 	});
