f8a0ec483bd4a1edf9d85230e41caa082678a321
Billuc <45941626+Billuc@users.noreply.github.com>
2024-04-01 14:12:26 +0000

    Deploying to gh-pages from @ Billuc/billuc.github.io@e3c25adac1cf451d522c30ee2498fc0e7edc4058 🚀

---
+++ _app/immutable/chunks/LinkButton.97a30de9.js
@@ -0,0 +1,23 @@
+import{S as k,i as y,s as A,E,k as _,a as F,y as j,l as p,m as v,c as B,z as I,h as c,n as d,b as L,F as g,A as S,G as q,H as x,I as z,g as b,d as w,B as C}from"./index.c1b86c61.js";import{F as D,i as G}from"./fa.9816df3d.js";function H(a){let o,r,i,l,f,u,s;const m=a[5].default,e=E(m,a,a[4],null);return l=new D({props:{icon:G,class:"animate-bounce"}}),{c(){o=_("a"),e&&e.c(),r=F(),i=_("div"),j(l.$$.fragment),this.h()},l(t){o=p(t,"A",{href:!0,class:!0,download:!0});var n=v(o);e&&e.l(n),r=B(n),i=p(n,"DIV",{class:!0});var h=v(i);I(l.$$.fragment,h),h.forEach(c),n.forEach(c),this.h()},h(){d(i,"class",f=`
+		${a[2]?"ml-2 group-hover:ml-6":"ml-4 group-hover:ml-8"}
+		duration-150 rotate-90
+	`),d(o,"href",a[0]),d(o,"class",u=`
+		rounded-full overflow-hidden 
+		flex justify-center items-center 
+		bg-stone-900 text-slate-100 hover:bg-slate-900 hover:text-pink-100
+		${a[2]?"border":"border-2"} border-stone-900 hover:border-pink-600
+		${a[2]?"py-0 px-6 hover:px-4":"py-1 px-8 hover:px-6"}
+		${a[2]?"text-sm":"text-base"}
+		group duration-150 ${a[3]}
+	`),d(o,"download",a[1])},m(t,n){L(t,o,n),e&&e.m(o,null),g(o,r),g(o,i),S(l,i,null),s=!0},p(t,[n]){e&&e.p&&(!s||n&16)&&q(e,m,t,t[4],s?z(m,t[4],n,null):x(t[4]),null),(!s||n&4&&f!==(f=`
+		${t[2]?"ml-2 group-hover:ml-6":"ml-4 group-hover:ml-8"}
+		duration-150 rotate-90
+	`))&&d(i,"class",f),(!s||n&1)&&d(o,"href",t[0]),(!s||n&12&&u!==(u=`
+		rounded-full overflow-hidden 
+		flex justify-center items-center 
+		bg-stone-900 text-slate-100 hover:bg-slate-900 hover:text-pink-100
+		${t[2]?"border":"border-2"} border-stone-900 hover:border-pink-600
+		${t[2]?"py-0 px-6 hover:px-4":"py-1 px-8 hover:px-6"}
+		${t[2]?"text-sm":"text-base"}
+		group duration-150 ${t[3]}
+	`))&&d(o,"class",u),(!s||n&2)&&d(o,"download",t[1])},i(t){s||(b(e,t),b(l.$$.fragment,t),s=!0)},o(t){w(e,t),w(l.$$.fragment,t),s=!1},d(t){t&&c(o),e&&e.d(t),C(l)}}}function N(a,o,r){let{$$slots:i={},$$scope:l}=o,{to:f}=o,{download:u=void 0}=o,{compact:s=void 0}=o,{class:m=""}=o;return a.$$set=e=>{"to"in e&&r(0,f=e.to),"download"in e&&r(1,u=e.download),"compact"in e&&r(2,s=e.compact),"class"in e&&r(3,m=e.class),"$$scope"in e&&r(4,l=e.$$scope)},[f,u,s,m,l,i]}class J extends k{constructor(o){super(),y(this,o,N,H,A,{to:0,download:1,compact:2,class:3})}}export{J as L};
