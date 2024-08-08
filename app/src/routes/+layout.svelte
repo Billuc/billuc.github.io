8331453982476738b7d95c81f828506b73a35082
Billuc <billuc@hotmail.fr>
2024-08-08 21:23:44 +0200

    feat(transition): added transition between pages

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-
 	import Layout from '$lib/components/layout/Layout.svelte';
+	import { fade } from 'svelte/transition';
+
+	export let data: { currentRoute: string };
 </script>
 
-<Layout>
-	<slot />
-</Layout>
+{#key data.currentRoute}
+	<main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+		<Layout>
+			<slot />
+		</Layout>
+	</main>
+{/key}
