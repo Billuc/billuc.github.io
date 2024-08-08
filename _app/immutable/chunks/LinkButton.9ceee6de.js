41f5d0a6e6e1965f97bb79ad554b0759465dd55e
Billuc <45941626+Billuc@users.noreply.github.com>
2024-08-08 19:51:59 +0000

    Deploying to gh-pages from @ Billuc/billuc.github.io@ecdc774af3e4123f8291734d2366dab0b8a01595 🚀

---
+++ _app/immutable/chunks/LinkButton.9ceee6de.js
@@ -0,0 +1,17 @@
+import{S as y,i as A,s as E,C as F,k as h,a as j,y as k,l as v,m as w,c as B,z as C,h as u,n as f,b as D,D as g,A as L,E as S,F as q,G as z,g as p,d as b,B as G}from"./index.7cfd5edb.js";import{F as I,r as N}from"./index.2284837b.js";function U(o){let a,i,r,l,c,d,s;const m=o[5].default,e=F(m,o,o[4],null);return l=new I({props:{icon:N}}),{c(){a=h("a"),e&&e.c(),i=j(),r=h("div"),k(l.$$.fragment),this.h()},l(t){a=v(t,"A",{href:!0,class:!0,download:!0});var n=w(a);e&&e.l(n),i=B(n),r=v(n,"DIV",{class:!0});var _=w(r);C(l.$$.fragment,_),_.forEach(u),n.forEach(u),this.h()},h(){f(r,"class",c=`${o[2]?"ml-2":"ml-4"} duration-150 rotate-90`),f(a,"href",o[0]),f(a,"class",d=`
+		rounded-sm overflow-hidden 
+		flex justify-center items-center 
+		transition-colors duration-200
+		bg-slate-900 text-slate-100 hover:text-red-300
+		${o[2]?"py-0 px-6":"py-1 px-8"}
+		${o[2]?"text-sm":"text-base"}
+		${o[3]}
+	`),f(a,"download",o[1])},m(t,n){D(t,a,n),e&&e.m(a,null),g(a,i),g(a,r),L(l,r,null),s=!0},p(t,[n]){e&&e.p&&(!s||n&16)&&S(e,m,t,t[4],s?z(m,t[4],n,null):q(t[4]),null),(!s||n&4&&c!==(c=`${t[2]?"ml-2":"ml-4"} duration-150 rotate-90`))&&f(r,"class",c),(!s||n&1)&&f(a,"href",t[0]),(!s||n&12&&d!==(d=`
+		rounded-sm overflow-hidden 
+		flex justify-center items-center 
+		transition-colors duration-200
+		bg-slate-900 text-slate-100 hover:text-red-300
+		${t[2]?"py-0 px-6":"py-1 px-8"}
+		${t[2]?"text-sm":"text-base"}
+		${t[3]}
+	`))&&f(a,"class",d),(!s||n&2)&&f(a,"download",t[1])},i(t){s||(p(e,t),p(l.$$.fragment,t),s=!0)},o(t){b(e,t),b(l.$$.fragment,t),s=!1},d(t){t&&u(a),e&&e.d(t),G(l)}}}function V(o,a,i){let{$$slots:r={},$$scope:l}=a,{to:c}=a,{download:d=void 0}=a,{compact:s=void 0}=a,{class:m=""}=a;return o.$$set=e=>{"to"in e&&i(0,c=e.to),"download"in e&&i(1,d=e.download),"compact"in e&&i(2,s=e.compact),"class"in e&&i(3,m=e.class),"$$scope"in e&&i(4,l=e.$$scope)},[c,d,s,m,l,r]}class K extends y{constructor(a){super(),A(this,a,V,U,E,{to:0,download:1,compact:2,class:3})}}export{K as L};
